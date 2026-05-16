6ed7cfbbbf3efa61c5e83511e4e3ed5456a9dae4
vidhi udasi <udasividhi2@gmail.com>
2026-05-16 15:30:38 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Remove Duplicates Sorted Array/remove-duplicates-sorted-array.java
+++ Difficulty: Easy/Remove Duplicates Sorted Array/remove-duplicates-sorted-array.java
@@ -1,43 +1,13 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.util.*;
-
-public class Main {
-    public static void main(String args[]) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int tc = Integer.parseInt(br.readLine());
-        while (tc-- > 0) {
-            String str[] = br.readLine().split(" ");
-            int arr[] = new int[str.length];
-            for (int i = 0; i < str.length; i++) arr[i] = Integer.parseInt(str[i]);
-            Solution obj = new Solution();
-            int len = obj.removeDuplicates(arr);
-            for (int i = 0; i < len; i++) {
-                System.out.print(arr[i] + " ");
-            }
-            System.out.println();
-            System.out.println("~");
-        }
-    }
-}
-// } Driver Code Ends
-
-
-// User function Template for Java
-
 class Solution {
-    // Function to remove duplicates from the given array
-    public int removeDuplicates(int[] arr) {
-        int n=arr.length;
-        int j=0;
-        for(int i=1;i<n;i++){
-            if(arr[j]!=arr[i]){
-                j++;
-                arr[j]=arr[i];
-                
-                
-            }
+    ArrayList<Integer> removeDuplicates(int[] arr) {
+        HashSet<Integer> set = new HashSet<>();
+        for(int a : arr) {
+            set.add(a);
         }
-        return j+1;
+        
+        Integer[] uniqueArr = set.toArray(new Integer[0]);
+        Arrays.sort(uniqueArr);
+        
+        return new ArrayList<>(Arrays.asList(uniqueArr));
     }
 }
