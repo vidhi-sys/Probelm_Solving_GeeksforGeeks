09ba0a99d6006b83accd950d4a202eca60924d7b
vidhi udasi <udasividhi2@gmail.com>
2026-06-13 22:48:08 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Nearly sorted/nearly-sorted.java
+++ Difficulty: Medium/Nearly sorted/nearly-sorted.java
@@ -1,7 +1,21 @@
+import java.util.*;
+
 class Solution {
     public void nearlySorted(int[] arr, int k) {
-        // code here
-        Arrays.sort(arr);//n-lo
-        
+
+        int idx = 0;
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        for (int ele : arr) {
+            pq.add(ele);
+
+            if (pq.size() > k) {
+                arr[idx++] = pq.remove();
+            }
+        }
+
+        while (!pq.isEmpty()) {
+            arr[idx++] = pq.remove();
+        }
     }
 }
