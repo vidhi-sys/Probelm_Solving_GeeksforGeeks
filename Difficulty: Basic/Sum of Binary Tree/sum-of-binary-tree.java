26af7953165a4d32c67e6cddae236035c626201a
vidhi udasi <udasividhi2@gmail.com>
2026-07-02 14:52:25 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Sum of Binary Tree/sum-of-binary-tree.java
+++ Difficulty: Basic/Sum of Binary Tree/sum-of-binary-tree.java
@@ -1,136 +1,25 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-import java.io.*;
-import java.util.*;
-import java.util.LinkedList;
-import java.util.Queue;
-
+/*
+Definition for Node
 class Node {
     int data;
     Node left;
     Node right;
 
-    Node(int data) {
-        this.data = data;
+    Node(int val) {
+        data = val;
         left = null;
         right = null;
-    }
-}
-
-class GfG {
-
-    static Node buildTree(String str) {
-
-        if (str.length() == 0 || str.charAt(0) == 'N') {
-            return null;
-        }
-
-        String ip[] = str.split(" ");
-        // Create the root of the tree
-        Node root = new Node(Integer.parseInt(ip[0]));
-        // Push the root to the queue
-
-        Queue<Node> queue = new LinkedList<>();
-
-        queue.add(root);
-        // Starting from the second element
-
-        int i = 1;
-        while (queue.size() > 0 && i < ip.length) {
-
-            // Get and remove the front of the queue
-            Node currNode = queue.peek();
-            queue.remove();
-
-            // Get the current node's value from the string
-            String currVal = ip[i];
-
-            // If the left child is not null
-            if (!currVal.equals("N")) {
-
-                // Create the left child for the current node
-                currNode.left = new Node(Integer.parseInt(currVal));
-                // Push it to the queue
-                queue.add(currNode.left);
-            }
-
-            // For the right child
-            i++;
-            if (i >= ip.length) break;
-
-            currVal = ip[i];
-
-            // If the right child is not null
-            if (!currVal.equals("N")) {
-
-                // Create the right child for the current node
-                currNode.right = new Node(Integer.parseInt(currVal));
-
-                // Push it to the queue
-                queue.add(currNode.right);
-            }
-            i++;
-        }
-
-        return root;
-    }
-
-    void inOrder(Node node) {
-        if (node == null) {
-            return;
-        }
-
-        inOrder(node.left);
-        System.out.print(node.data + " ");
-
-        inOrder(node.right);
-    }
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int t = Integer.parseInt(br.readLine());
-
-        while (t-- > 0) {
-            String s = br.readLine();
-            Node root = buildTree(s);
-            Solution g = new Solution();
-            int result = g.sumBT(root);
-            System.out.println(result);
-
-            System.out.println("~");
-        }
-    }
-}
-
-// } Driver Code Ends
-
-
-// User function Template for Java
-
-/*
-// A Binary Tree node
-class Node
-{
-    int data;
-    Node left, right;
-
-    Node(int item)
-    {
-        data = item;
-        left = right = null;
     }
 }
 */
 
 class Solution {
-    // Function to return sum of all nodes of a binary tree
     static int sumBT(Node root) {
-        if(root==null){
+     if (root == null)
             return 0;
-        }
-        return root.data+sumBT(root.left)+sumBT(root.right);
+
+        return (root.data + sumBT(root.left)
+                + sumBT(root.right));
         
     }
 }
