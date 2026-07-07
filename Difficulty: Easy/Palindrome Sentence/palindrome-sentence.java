146d4b78225a6c16d2961846e8072375f7447be6
vidhi udasi <udasividhi2@gmail.com>
2026-07-07 10:17:12 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Palindrome Sentence/palindrome-sentence.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isPalinSent(String s) {
+        int i = 0;
+        int j = s.length() - 1;
+
+        while (i < j) {
+
+            while (i < j && !Character.isLetterOrDigit(s.charAt(i))) {
+                i++;
+            }
+
+            while (i < j && !Character.isLetterOrDigit(s.charAt(j))) {
+                j--;
+            }
+
+            if (Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j))) {
+                return false;
+            }
+
+            i++;
+            j--;
+        }
+
+        return true;
+    }
+}
