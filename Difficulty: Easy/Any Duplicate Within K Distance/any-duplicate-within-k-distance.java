062284ad618463f91af8ac211f995f723d728b66
vidhi udasi <udasividhi2@gmail.com>
2026-08-16 16:34:03 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Any Duplicate Within K Distance/any-duplicate-within-k-distance.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean checkDuplicatesWithinK(int[] arr, int k) {
+        // your code
+        HashSet<Integer> set = new HashSet<>();
+
+        // Traverse the input array
+        for (int i=0; i<arr.length; i++)
+        {
+            // If already present n hash, then we found 
+            // a duplicate within k distance
+            if (set.contains(arr[i]))
+               return true;
+
+            // Add this item to hashset
+            set.add(arr[i]);
+
+            // Remove the k+1 distant item
+            if (i >= k)
+              set.remove(arr[i-k]);
+        }
+        return false;
+    }
+}
